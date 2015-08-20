e706a79c28a9785089096a13cc09336c5bb1ef23
Michael Jackson <mjijackson@gmail.com>
2015-08-19 21:45:48 -0700

    Add gitbookConfigURL config

--- README.md
+++ README.md
@@ -10,7 +10,8 @@ Put this in your book.json:
 {
   "plugins": [ "versions" ],
   "pluginsConfig": {
-    "github": {
+    "versions": {
+      "gitbookConfigURL": "https://raw.githubusercontent.com/rackt/history/gh-pages/book.json",
       "options": [
         { "value": "https://rackt.github.io/history/v1.3.0/", "text": "Version 1.3.0" },
         { "value": "https://rackt.github.io/history/v1.4.0/", "text": "Version 1.4.0", "selected": true }
@@ -20,6 +21,6 @@ Put this in your book.json:
 }
 ```
 
-When the user selects one of the options, they are taken to that URL.
+A `<select>` element will be created with the given `options` and placed at the top of the book summary. When the user selects one of the options, they are taken to that URL.
 
-And you're done!
+The `gitbookConfigURL` variable is a publicly accessible URL to your `book.json`. If this is present, the plugin will fetch the latest config when the page loads, so even older versions of your book will have updated `options`.
--- assets/plugin.js
+++ assets/plugin.js
@@ -1,8 +1,9 @@
 require([ 'gitbook' ], function (gitbook) {
-  var options;
+  var options, selectedValue;
 
   function createOptionHTML(option) {
-    return '<option value="' + option.value + '"' + (option.selected ? ' selected' : '') + '>' + option.text + '</option>';
+    var isSelected = selectedValue ? selectedValue === option.value : !!option.selected;
+    return '<option value="' + option.value + '"' + (isSelected ? ' selected' : '') + '>' + option.text + '</option>';
   }
 
   function insertVersionsSelect() {
@@ -27,7 +28,25 @@ require([ 'gitbook' ], function (gitbook) {
 
   gitbook.events.bind('start', function (e, config) {
     options = config.versions.options;
+
+    if (options)
+      for (var i = 0; selectedValue == null && i < options.length; ++i)
+        if (options[i].selected)
+          selectedValue = options[i].value;
+
     insertVersionsSelect();
+
+    // Make sure we have a current book.json
+    if (config.versions.gitbookConfigURL) {
+      jQuery.getJSON(config.versions.gitbookConfigURL, function (bookConfig) {
+        options = bookConfig.pluginsConfig.versions.options;
+
+        // Update .versions-select with all options, preserving
+        // the option that was selected in this build's config
+        jQuery('.versions-select').remove();
+        insertVersionsSelect();
+      });
+    }
   });
 
   gitbook.events.bind('page.change', function () {
